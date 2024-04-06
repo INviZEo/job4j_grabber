f6accc54b4aad228eda72fd435331e980048ed5f
INviZEo <lev19825@mail.ru>
2024-04-06 21:07:24 +0300

    2. Парсинг HTML страницы. [#260358 #490084]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -14,10 +14,11 @@ public class HabrCareerParse {
     private static final String SOURCE_LINK = "https://career.habr.com";
     public static final String PREFIX = "/vacancies?page=";
     public static final String SUFFIX = "&q=Java%20developer&type=all";
+    private static final int PAGENUMBER = 5;
 
     public static void main(String[] args) throws IOException {
-        for (int pageNumber = 1; pageNumber < 6; pageNumber++) {
-            String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
+        for (int i = 1; i <= PAGENUMBER; i++) {
+            String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, PAGENUMBER, SUFFIX);
             Connection connection = Jsoup.connect(fullLink);
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
